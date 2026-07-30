b9fbf9fadbe96bb93a16e7075e0416755498227b
Preethi-07-Srinivasan <preethi.seenu.07@gmail.com>
2026-07-30 07:24:39 +0530

    Delete Week 7/Week 7 directory

--- Week 7/Week 7/4.Template-Driven Forms & Validation/src/app/directives/card-hover.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CardHover } from './card-hover';
-
-describe('CardHover', () => {
-  it('should create an instance', () => {
-    const directive = new CardHover();
-    expect(directive).toBeTruthy();
-  });
-});
--- Week 7/Week 7/4.Template-Driven Forms & Validation/src/app/pipes/grade-status-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GradeStatusPipe } from './grade-status-pipe';
-
-describe('GradeStatusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GradeStatusPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
